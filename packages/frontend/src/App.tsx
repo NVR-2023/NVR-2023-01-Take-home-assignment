200af93cf09c4a871673f15468e3e6d3aa906a73
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-11-11 22:10:35 +0000

    feat: install react via vite and tailwind css

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  const test: string ="123";
+
+  return (
+    <>
+      <p className="text-blue-600 bg-yellow-400"> {test}</p>
+    </>
+  );
+}
+
+export default App;
